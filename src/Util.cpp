11ad885643745e5ab04ed43f32628a566c87e58d
Jestin <cherian.jestin@gmail.com>
2020-10-10 01:08:20 -0400

    Added C++ Project Baseline

---
+++ src/Util.cpp
@@ -0,0 +1,23 @@
+#include "Util.h"
+
+Util::Util()
+{
+    //ctor
+}
+
+Util::~Util()
+{
+    //dtor
+}
+
+Util::Util(const Util& other)
+{
+    //copy ctor
+}
+
+Util& Util::operator=(const Util& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
